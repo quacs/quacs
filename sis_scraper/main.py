b568d00147707152fa28287178b4edbb8738d26b
Eli Schiff <eli@elischiff.org>
2020-05-31 14:43:09 -0400

    added conflicts directly into sis_scraper so that they are all precalculated

--- sis_scraper/main.py
+++ sis_scraper/main.py
@@ -7,6 +7,49 @@ import re
 
 load_dotenv()
 
+def addConflicts(data):
+    for department in data['departments']:
+        for course in department['courses']:
+            for section in course['sections']:
+                section['conflicts'] = getConflict(data, section['timeslots'], section['subj']+str(section['crse']))
+
+def getConflict(data, check_timeslots, course_code):
+    conflicts = {}
+
+    for department in data['departments']:
+        for course in department['courses']:
+            for section in course['sections']:
+                for timeslot in section['timeslots']:
+                    for day in timeslot['days']:
+                        # Dont conflict with other sections of the same course (or with self)
+                        if course_code == section['subj']+str(section['crse']):
+                            continue
+
+                        # If this course does not have a timeslot just skip it
+                        if timeslot['time_start'] == -1 or timeslot['time_end'] == -1:
+                            continue
+
+                        for check_timeslot in check_timeslots:
+                            # If this course does not have a timeslot just skip it
+                            if check_timeslot['time_start'] == -1 or check_timeslot['time_end'] == -1:
+                                continue
+
+                            # If not happening on the same day skip it
+                            if day not in check_timeslot['days']:
+                                continue
+
+                            # If the dates dont overlap skip it
+                            if not max(check_timeslot['date_start'], timeslot['date_start']) < min(check_timeslot['date_end'], timeslot['date_end']):
+                                continue
+
+                            # There is a conflict
+                            if max(check_timeslot['time_start'], timeslot['time_start']) < min(check_timeslot['time_end'], timeslot['time_end']):
+                                # JSON does not support hashtables without a value so the value
+                                # is always set to true even though just by being in the conflicts
+                                # hash table is enough to know it conflicts
+                                conflicts[section['crn']] = True
+
+    return conflicts
 
 
 def getContent(element):
@@ -52,6 +95,10 @@ with requests.Session() as s:
 
     url = "https://sis.rpi.edu/rss/bwskfcls.P_GetCrse_Advanced"
     payload = f'rsts=dummy&crn=dummy&term_in={os.getenv("CURRENT_TERM")}&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=ADMN&sel_subj=USAF&sel_subj=ARCH&sel_subj=ARTS&sel_subj=ASTR&sel_subj=BCBP&sel_subj=BIOL&sel_subj=BMED&sel_subj=CHME&sel_subj=CHEM&sel_subj=CIVL&sel_subj=COGS&sel_subj=COMM&sel_subj=CSCI&sel_subj=ENGR&sel_subj=ERTH&sel_subj=ECON&sel_subj=ECSE&sel_subj=ESCI&sel_subj=ENVE&sel_subj=GSAS&sel_subj=ISYE&sel_subj=ITWS&sel_subj=IENV&sel_subj=IHSS&sel_subj=ISCI&sel_subj=LANG&sel_subj=LGHT&sel_subj=LITR&sel_subj=MGMT&sel_subj=MTLE&sel_subj=MATP&sel_subj=MATH&sel_subj=MANE&sel_subj=USAR&sel_subj=USNA&sel_subj=PHIL&sel_subj=PHYS&sel_subj=PSYC&sel_subj=STSH&sel_subj=STSS&sel_subj=WRIT&sel_crse=&sel_title=&sel_from_cred=&sel_to_cred=&sel_camp=%25&sel_ptrm=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a&SUB_BTN=Section+Search&path=1'
+
+    # This payload is for testing. It will only return CSCI classes and will therefore be a bit faster
+    # payload = f'rsts=dummy&crn=dummy&term_in={os.getenv("CURRENT_TERM")}&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=CSCI&sel_crse=&sel_title=&sel_from_cred=&sel_to_cred=&sel_camp=%25&sel_ptrm=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a&SUB_BTN=Section+Search&path=1'
+
     headers = {
     }
     response = s.request("POST", url, headers=headers, data = payload)
@@ -83,27 +130,27 @@ with requests.Session() as s:
             td = row.findAll("td")
             if "TBA" not in getContent(td[8]):
                 timeslot_data = {
-                    "Days":list(getContent(td[8])),
-                    "Time_start":timeToMilitary(getContentFromChild(td[9], 'abbr'), True),
-                    "Time_end":timeToMilitary(getContentFromChild(td[9], 'abbr'), False),
-                    "Instructor":cleanOutAbbr(getContent(td[19])),
-                    "Date_start":getContentFromChild(td[20], 'abbr').split('-')[0],
-                    "Date_end":getContentFromChild(td[20], 'abbr').split('-')[1],
-                    "Location":getContentFromChild(td[21], 'abbr')
+                    "days":list(getContent(td[8])),
+                    "time_start":timeToMilitary(getContentFromChild(td[9], 'abbr'), True),
+                    "time_end":timeToMilitary(getContentFromChild(td[9], 'abbr'), False),
+                    "instructor":cleanOutAbbr(getContent(td[19])),
+                    "date_start":getContentFromChild(td[20], 'abbr').split('-')[0],
+                    "date_end":getContentFromChild(td[20], 'abbr').split('-')[1],
+                    "location":getContentFromChild(td[21], 'abbr')
                 }
             else:
                 timeslot_data = {
-                    "Date_end": "",
-                    "Date_start": "",
-                    "Days": [],
-                    "Instructor": "",
-                    "Location": "",
-                    "Time_end": -1,
-                    "Time_start": -1
+                    "date_end": "",
+                    "date_start": "",
+                    "days": [],
+                    "instructor": "",
+                    "location": "",
+                    "time_end": -1,
+                    "time_start": -1
                 }
 
             if len(getContent(td[0])) == 0:
-                data['departments'][-1]['courses'][-1]['Sections'][-1]['timeslots'].append(timeslot_data)
+                data['departments'][-1]['courses'][-1]['sections'][-1]['timeslots'].append(timeslot_data)
                 continue;
 
 
@@ -113,42 +160,44 @@ with requests.Session() as s:
                 credit_max = float(getContent(td[6]).split('-')[1])
 
             section_data = {
-                "Select":getContentFromChild(td[0], 'abbr'),
-                "CRN":int(getContentFromChild(td[1], 'a')),
-                "Subj":getContent(td[2]),
-                "Crse":int(getContent(td[3])),
-                "Sec":getContent(td[4]),
-                "Cmp":getContent(td[5]),
-                "Cred_min":credit_min,
-                "Cred_max":credit_max,
-                "Title":getContent(td[7]).title(),
-                "Cap":int(getContent(td[10])),
-                "Act":int(getContent(td[11])),
-                "Rem":int(getContent(td[12])),
-                "WL_Cap":int(getContent(td[13])),
-                "WL_Act":int(getContent(td[14])),
-                "WL_Rem":int(getContent(td[15])),
-                "XL_Cap":getContent(td[16]),
-                "XL_Act":getContent(td[17]),
-                "XL_Rem":getContent(td[18]),
-                "Attribute":getContent(td[22]) if 22 < len(td) else "",
+                "select":getContentFromChild(td[0], 'abbr'),
+                "crn":int(getContentFromChild(td[1], 'a')),
+                "subj":getContent(td[2]),
+                "crse":int(getContent(td[3])),
+                "sec":getContent(td[4]),
+                "cmp":getContent(td[5]),
+                "cred_min":credit_min,
+                "cred_max":credit_max,
+                "title":getContent(td[7]).title(),
+                "cap":int(getContent(td[10])),
+                "act":int(getContent(td[11])),
+                "rem":int(getContent(td[12])),
+                "wl_cap":int(getContent(td[13])),
+                "wl_act":int(getContent(td[14])),
+                "wl_rem":int(getContent(td[15])),
+                "xl_cap":getContent(td[16]),
+                "xl_act":getContent(td[17]),
+                "xl_rem":getContent(td[18]),
+                "attribute":getContent(td[22]) if 22 < len(td) else "",
                 "timeslots":[timeslot_data]
             }
 
-            if section_data['Subj'] == last_subject and section_data['Crse'] == last_course_code:
-                data['departments'][-1]['courses'][-1]['Sections'].append(section_data)
+            if section_data['subj'] == last_subject and section_data['crse'] == last_course_code:
+                data['departments'][-1]['courses'][-1]['sections'].append(section_data)
                 continue;
 
             last_subject = getContent(td[2])
             last_course_code = int(getContent(td[3]))
             data['departments'][-1]['courses'].append({
-                "Title":getContent(td[7]),
-                "Subj":getContent(td[2]),
-                "Crse":int(getContent(td[3])),
-                "Sections":[section_data]
+                "title":getContent(td[7]),
+                "subj":getContent(td[2]),
+                "crse":int(getContent(td[3])),
+                "sections":[section_data]
             })
 
             if len(getContent(td[2])) > 0:
                 data['departments'][-1]['code'] = getContent(td[2])
 
-    print(json.dumps(data, indent=4, sort_keys=True))
+    addConflicts(data)
+
+    print(json.dumps(data))
