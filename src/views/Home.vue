a2f02f6e108d7d4441c8643cb226c6acfdf88a9a
Benjamin Sherman <benjamin@bensherman.io>
2020-06-06 18:49:20 -0400

    Combine Homepage.vue and Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="home">
-    <Homepage />
+    <h3>
+      Join the QuACS development discord server!
+      <a href="https://discord.gg/3xNxfBy">https://discord.gg/3xNxfBy</a>
+    </h3>
+    <br /><br />
+    <div v-for="department in departments" v-bind:key="department.code">
+      <router-link class="nav-link" :to="'/department/' + department.code"
+        >{{ department.code }}: {{ department.name }}</router-link
+      >
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Homepage from "@/components/Homepage.vue";
+import { mapState } from "vuex";
 
 export default {
   name: "Home",
-  components: {
-    Homepage
-  }
+  computed: mapState(["departments"])
 };
 </script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+  text-align: left;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  text-align: left;
+}
+</style>
