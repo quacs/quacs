e7fb39068e7ebe7198b46ce90dc7491780ebf4bc
Benjamin Sherman <benjamin@bensherman.io>
2020-06-06 22:06:51 -0400

    Fix CI

---
+++ scripts/gh-pages-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -x
+
+git checkout --orphan gh-pages || exit 1
+yarn build || exit 1
+git --work-tree dist add --all || exit 1
+git --work-tree dist commit -m "$(date -u)" || exit 1
+exit 1
+git push origin HEAD:gh-pages --force || exit 1
+rm -r dist
+git checkout -f master || exit 1
+git branch -D gh-pages || exit 1
