cbf31d904f5a9dccb10f8f79b397141579d579e3
Benjamin Sherman <benjamin@bensherman.io>
2020-06-06 22:09:01 -0400

    Remove bail from CI script

--- scripts/gh-pages-deploy.sh
+++ scripts/gh-pages-deploy.sh
@@ -6,7 +6,6 @@ git checkout --orphan gh-pages || exit 1
 yarn build || exit 1
 git --work-tree dist add --all || exit 1
 git --work-tree dist commit -m "$(date -u)" || exit 1
-exit 1
 git push origin HEAD:gh-pages --force || exit 1
 rm -r dist
 git checkout -f master || exit 1
