a3dfcfec64c2b45a3fa7e969ce8fbaa878f0aa8a
Benjamin Sherman <benjamin@bensherman.io>
2020-06-07 23:37:07 -0400

    Fix scolling between pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,7 +22,14 @@ const routes: Array<RouteConfig> = [
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
 });
 
 export default router;
