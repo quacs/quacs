5c59ce78b2d42da49ced037993195a4acb046b71
Benjamin Sherman <benjamin@bensherman.io>
2020-06-14 01:11:33 -0400

    Convert to optimized code

--- src/lib.rs
+++ src/lib.rs
@@ -1,155 +1,101 @@
 #[macro_use]
-extern crate serde_derive;
-
-extern crate wasm_bindgen;
-extern crate web_sys;
-
-#[macro_use]
 mod utils;
-
-mod types;
-use types::CrnToSec;
-
-use std::collections::{HashMap, HashSet};
+mod parsed;
+use parsed::TIMES;
 
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn init() {
-    #[cfg(feature = "console_error_panic_hook")]
-    console_error_panic_hook::set_once();
-}
+#[wasm_bindgen(js_name = "generateSchedulesAndConflicts")]
+pub fn generate_schedules_and_conflicts(selected_courses: &JsValue) -> Box<[u64]> {
+    let mut selected_courses: Vec<Vec<u32>> = selected_courses.into_serde().unwrap();
+    selected_courses.sort_by_key(|vec| vec.len());
 
-#[wasm_bindgen(js_name = generateCurrentSchedulesAndConflicts)]
-pub fn gen_schedules_and_conflicts(
-    selected_crns: &JsValue,
-    crn_to_sections: &JsValue,
-) -> Box<[u32]> {
-    bm_start!("parse inputs");
-    let selected_crns: HashMap<u32, bool> = selected_crns.into_serde().unwrap();
+    console_log!("Selected courses: {:#?}", selected_courses);
 
-    let crn_to_sections: HashMap<u32, CrnToSec> = crn_to_sections.into_serde().unwrap();
-    bm_end!("parse inputs");
-
-    bm_start!("calculation");
-    let mut selected_courses: HashMap<&String, Vec<u32>> = HashMap::new();
-
-    // Map selected sections to courses
-    for (course, section) in selected_crns
-        .iter()
-        .filter(|(_, selected)| **selected)
-        .map(|(crn, _)| (&crn_to_sections[crn].course, crn))
-    {
-        match selected_courses.get_mut(&course) {
-            Some(course_vec) => course_vec.push(*section),
-            None => {
-                selected_courses.insert(&course, vec![*section]);
-            }
-        };
-    }
-
-    let selected_courses: Vec<&Vec<u32>> = selected_courses.values().collect();
-
-    let mut conflicts = HashSet::new();
-    let schedules = gen_schedules(
+    bm_start!("generate schedules");
+    let mut times = [0; 3];
+    let schedules = generate_schedules(
         0,
         &selected_courses,
-        &mut Vec::new(),
-        &mut conflicts,
-        &crn_to_sections,
+        &mut Vec::with_capacity(selected_courses.len()),
+        &mut [0; 3],
+        &mut times,
     );
-    let magic_num_vec = vec![selected_courses.len() as u32, conflicts.len() as u32];
+    bm_end!("generate schedules");
 
-    let ret = magic_num_vec
-        .into_iter()
-        .chain(conflicts.into_iter())
-        .chain(schedules.into_iter().flatten())
-        .collect();
-    bm_end!("calculation");
+    console_log!("Generated {} schedules", schedules.len());
 
-    ret
+    let total_size = schedules.len() * selected_courses.len() + 3;
+
+    let mut ret = Vec::with_capacity(total_size);
+
+    for time in &times {
+        ret.push(*time);
+    }
+
+    for schedule in &schedules {
+        for crn in schedule {
+            ret.push(*crn as u64);
+        }
+    }
+
+    console_log!("Output array size: {}", ret.len());
+
+    ret.into_boxed_slice()
 }
 
-fn gen_schedules(
+fn generate_schedules(
     idx: usize,
-    selected_crns: &Vec<&Vec<u32>>,
-    used_sections: &mut Vec<u32>,
-    overall_conflicts: &mut HashSet<u32>,
-    crn_to_sections: &HashMap<u32, CrnToSec>,
+    courses: &Vec<Vec<u32>>,
+    current_courses: &mut Vec<u32>,
+    current_times: &mut [u64; 3],
+    overall_times: &mut [u64; 3],
 ) -> Vec<Vec<u32>> {
-    if idx >= selected_crns.len() {
-        if overall_conflicts.is_empty() {
-            let initial_conflicts = generate_initial_conflicts(used_sections, crn_to_sections);
-
-            overall_conflicts.reserve(initial_conflicts.len());
-            initial_conflicts.iter().for_each(|crn| {
-                overall_conflicts.insert(*crn);
-            });
-        } else {
-            let curr_schedule_conflicts =
-                generate_conflicts(used_sections, crn_to_sections, overall_conflicts);
-
-            overall_conflicts.retain(|conf| curr_schedule_conflicts.contains(conf));
+    if idx >= courses.len() {
+        for i in 0..3 {
+            overall_times[i] |= current_times[i];
         }
 
-        return vec![used_sections.iter().map(|i| *i).collect()];
+        return vec![current_courses.clone()];
     }
 
     let mut ret = Vec::new();
 
-    for crn in selected_crns[idx].iter() {
-        if crn_to_sections[crn]
-            .conflicts
-            .iter()
-            .any(|crn| used_sections.contains(crn))
-        {
+    for section in &courses[idx] {
+        let curr_sec_times = TIMES.get(section).unwrap();
+        let section_conflicts = bitwise_and(TIMES.get(section).unwrap(), current_times);
+        if section_conflicts.iter().any(|cnf| cnf != &0) {
             continue;
         }
 
-        used_sections.push(*crn);
+        bitwise_xor_mut(current_times, curr_sec_times);
+        current_courses.push(*section);
 
-        ret.append(&mut gen_schedules(
+        ret.append(&mut generate_schedules(
             idx + 1,
-            selected_crns,
-            used_sections,
-            overall_conflicts,
-            crn_to_sections,
+            courses,
+            current_courses,
+            current_times,
+            overall_times,
         ));
 
-        used_sections.pop();
+        current_courses.pop();
+        bitwise_xor_mut(current_times, curr_sec_times);
     }
 
-    return ret;
+    ret
 }
 
-fn generate_conflicts(
-    schedule: &Vec<u32>,
-    crn_to_sections: &HashMap<u32, CrnToSec>,
-    overall_conflicts: &HashSet<u32>,
-) -> Vec<u32> {
-    schedule
-        .iter()
-        .map(|crn| &crn_to_sections[crn])
-        .map(|section| {
-            section
-                .conflicts
-                .iter()
-                .filter(|crn| overall_conflicts.contains(crn))
-        })
-        .flatten()
-        .map(|i| *i)
-        .collect()
+fn bitwise_and(t1: &[u64; 3], t2: &[u64; 3]) -> [u64; 3] {
+    let mut ret = [0; 3];
+    for i in 0..3 {
+        ret[i] = t1[i] & t2[i];
+    }
+    ret
 }
 
-fn generate_initial_conflicts(
-    schedule: &Vec<u32>,
-    crn_to_sections: &HashMap<u32, CrnToSec>,
-) -> Vec<u32> {
-    schedule
-        .iter()
-        .map(|crn| &crn_to_sections[crn])
-        .map(|section| section.conflicts.iter())
-        .flatten()
-        .map(|i| *i)
-        .collect()
+fn bitwise_xor_mut(t1: &mut [u64; 3], t2: &[u64; 3]) {
+    for i in 0..3 {
+        t1[i] ^= t2[i];
+    }
 }
