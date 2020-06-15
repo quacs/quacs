72064f61dba9ae9164fd03fe7405cdec06264ec1
Eli Schiff <eli@elischiff.org>
2020-06-15 00:19:52 -0400

    Add staging branch deploy code

--- scripts/gh-pages-deploy.sh
+++ scripts/gh-pages-deploy.sh
@@ -2,12 +2,21 @@
 
 set -x
 
-git checkout --orphan gh-pages || exit 1
+# If current branch == staging
+if git rev-parse --abbrev-ref HEAD | grep -w staging > /dev/null; then
+  BRANCH="gh-pages-staging"
+  CNAME="staging.quacs.org"
+else
+  BRANCH="gh-pages"
+  CNAME="quacs.org"
+fi
+
+git checkout --orphan ${BRANCH} || exit 1
 yarn build || exit 1
-echo 'quacs.org' > dist/CNAME
+echo ${CNAME} > dist/CNAME
 git --work-tree dist add --all || exit 1
 git --work-tree dist commit -m "$(date -u)" || exit 1
-git push origin HEAD:gh-pages --force || exit 1
+git push origin HEAD:${BRANCH} --force || exit 1
 rm -r dist
 git checkout -f master || exit 1
-git branch -D gh-pages || exit 1
+git branch -D ${BRANCH} || exit 1
