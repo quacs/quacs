0a8840479619203a18d15bd18ca04bc0dc461ac9
Benjamin Sherman <benjamin@bensherman.io>
2020-06-15 18:58:40 -0400

    Use debug build on staging branch

--- scripts/gh-pages-deploy.sh
+++ scripts/gh-pages-deploy.sh
@@ -7,14 +7,16 @@ if git rev-parse --abbrev-ref HEAD | grep -w staging > /dev/null; then
   BRANCH="staging"
   PAGESBRANCH="gh-pages-staging"
   CNAME="staging.quacs.org"
+	ACTION="debug-build"
 else
   BRANCH="master"
   PAGESBRANCH="gh-pages"
   CNAME="quacs.org"
+	ACTION="build"
 fi
 
 git checkout --orphan ${PAGESBRANCH} || exit 1
-yarn build || exit 1
+yarn ${ACTION} || exit 1
 echo ${CNAME} > dist/CNAME
 git --work-tree dist add --all || exit 1
 git --work-tree dist commit -m "$(date -u)" || exit 1
