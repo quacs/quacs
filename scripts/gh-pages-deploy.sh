6e84dd50182e33a7632e9520db7d0e314656088a
Eli Schiff <eli@elischiff.org>
2020-06-15 00:24:25 -0400

    fix deploy script

--- scripts/gh-pages-deploy.sh
+++ scripts/gh-pages-deploy.sh
@@ -4,19 +4,21 @@ set -x
 
 # If current branch == staging
 if git rev-parse --abbrev-ref HEAD | grep -w staging > /dev/null; then
-  BRANCH="gh-pages-staging"
+  BRANCH="staging"
+  PAGESBRANCH="gh-pages-staging"
   CNAME="staging.quacs.org"
 else
-  BRANCH="gh-pages"
+  BRANCH="master"
+  PAGESBRANCH="gh-pages"
   CNAME="quacs.org"
 fi
 
-git checkout --orphan ${BRANCH} || exit 1
+git checkout --orphan ${PAGESBRANCH} || exit 1
 yarn build || exit 1
 echo ${CNAME} > dist/CNAME
 git --work-tree dist add --all || exit 1
 git --work-tree dist commit -m "$(date -u)" || exit 1
-git push origin HEAD:${BRANCH} --force || exit 1
+git push origin HEAD:${PAGESBRANCH} --force || exit 1
 rm -r dist
-git checkout -f master || exit 1
-git branch -D ${BRANCH} || exit 1
+git checkout -f ${BRANCH} || exit 1
+git branch -D ${PAGESBRANCH} || exit 1
