2e917b39c65044a09808de44bade2ff8d76c260a
Benjamin Sherman <benjamin@bensherman.io>
2020-11-01 13:31:31 -0500

    Use aiohttp in prerequisites scraper

--- prerequisites_scraper/main.py
+++ prerequisites_scraper/main.py
@@ -3,7 +3,7 @@ import re
 from typing import List, Tuple
 
 import bs4
-import requests
+import aiohttp
 
 # The token format is (token_name, token_text)
 Token = Tuple[str, str]
@@ -107,119 +107,121 @@ def parse(prereqs: str):
     return result
 
 
-def get_prereq_string(term, crn):
-    r = requests.get(
+async def get_prereq_string(s, term, crn):
+    async with s.get(
         f"https://sis.rpi.edu/rss/bwckschd.p_disp_detail_sched?term_in={term}&crn_in={crn}"
-    )
-    soup = bs4.BeautifulSoup(r.text, features="lxml")
+    ) as r:
+        soup = bs4.BeautifulSoup(await r.text("utf8"), features="lxml")
 
-    el = soup.find(
-        attrs={"summary": "This layout table is used to present the seating numbers."}
-    )
-    el = el.next_sibling
-
-    section = ""
-    data = {}
-    while el:
-        if el.string:
-            if el.name == "span":
-                section = "_".join(el.string.lower().split())
-                section = "".join([i for i in section if i.isalpha() or i == "_"])
-                if section not in data:
-                    data[section] = []
-                el = el.next_sibling
-                continue
-
-            if section:
-                if el.string.strip():
-                    data[section].append(el.string.strip())
-
+        el = soup.find(
+            attrs={
+                "summary": "This layout table is used to present the seating numbers."
+            }
+        )
         el = el.next_sibling
 
-    if "prerequisites" in data:
-        data["prerequisites"] = parse(" ".join(data["prerequisites"]))
-
-    if "corequisites" in data:
-        data["corequisites"] = [
-            "-".join(course.split()) for course in data["corequisites"]
-        ]
-
-    if "cross_list_courses" in data:
-        data["cross_list_courses"] = [
-            "-".join(course.split()) for course in data["cross_list_courses"]
-        ]
-
-    if "restrictions" in data:
-        data["restrictions_clean"] = {}
         section = ""
-        subsection = ""
-        for part in data["restrictions"]:
-            if part.endswith("Majors:"):
-                section = "major"
-                data["restrictions_clean"][section] = {}
-            elif part.endswith("Levels:"):
-                section = "level"
-                data["restrictions_clean"][section] = {}
-            elif part.endswith("Classifications:"):
-                section = "classification"
-                data["restrictions_clean"][section] = {}
-            elif part.endswith("or Concentration):"):
-                section = "field_of_study"
-                data["restrictions_clean"][section] = {}
-            elif part.endswith("Degrees:"):
-                section = "degree"
-                data["restrictions_clean"][section] = {}
-            elif part.endswith("Colleges:"):
-                section = "college"
-                data["restrictions_clean"][section] = {}
-            elif part.endswith("Campuses:"):
-                section = "campus"
-                data["restrictions_clean"][section] = {}
+        data = {}
+        while el:
+            if el.string:
+                if el.name == "span":
+                    section = "_".join(el.string.lower().split())
+                    section = "".join([i for i in section if i.isalpha() or i == "_"])
+                    if section not in data:
+                        data[section] = []
+                    el = el.next_sibling
+                    continue
 
-            if part.startswith("Must be enrolled"):
-                subsection = "must_be"
-                data["restrictions_clean"][section]["must_be"] = []
-                continue
-            elif part.startswith("May not be enrolled"):
-                subsection = "may_not_be"
-                data["restrictions_clean"][section]["may_not_be"] = []
-                continue
+                if section:
+                    if el.string.strip():
+                        data[section].append(el.string.strip())
 
-            if section:
-                data["restrictions_clean"][section][subsection].append(part)
-        data["restrictions"] = data["restrictions_clean"]
-        del data["restrictions_clean"]
+            el = el.next_sibling
 
-    print(json.dumps(data, indent=4))
-    return data
+        if "prerequisites" in data:
+            data["prerequisites"] = parse(" ".join(data["prerequisites"]))
+
+        if "corequisites" in data:
+            data["corequisites"] = [
+                "-".join(course.split()) for course in data["corequisites"]
+            ]
+
+        if "cross_list_courses" in data:
+            data["cross_list_courses"] = [
+                "-".join(course.split()) for course in data["cross_list_courses"]
+            ]
+
+        if "restrictions" in data:
+            data["restrictions_clean"] = {}
+            section = ""
+            subsection = ""
+            for part in data["restrictions"]:
+                if part.endswith("Majors:"):
+                    section = "major"
+                    data["restrictions_clean"][section] = {}
+                elif part.endswith("Levels:"):
+                    section = "level"
+                    data["restrictions_clean"][section] = {}
+                elif part.endswith("Classifications:"):
+                    section = "classification"
+                    data["restrictions_clean"][section] = {}
+                elif part.endswith("or Concentration):"):
+                    section = "field_of_study"
+                    data["restrictions_clean"][section] = {}
+                elif part.endswith("Degrees:"):
+                    section = "degree"
+                    data["restrictions_clean"][section] = {}
+                elif part.endswith("Colleges:"):
+                    section = "college"
+                    data["restrictions_clean"][section] = {}
+                elif part.endswith("Campuses:"):
+                    section = "campus"
+                    data["restrictions_clean"][section] = {}
+
+                if part.startswith("Must be enrolled"):
+                    subsection = "must_be"
+                    data["restrictions_clean"][section]["must_be"] = []
+                    continue
+                elif part.startswith("May not be enrolled"):
+                    subsection = "may_not_be"
+                    data["restrictions_clean"][section]["may_not_be"] = []
+                    continue
+
+                if section:
+                    data["restrictions_clean"][section][subsection].append(part)
+            data["restrictions"] = data["restrictions_clean"]
+            del data["restrictions_clean"]
+
+        return data
+
+
+async def main():
+    async with aiohttp.ClientSession() as s:
+        for term in os.listdir("data"):
+            prerequisites = {}
+
+            crns = []
+            with open(f"data/{term}/courses.json") as json_file:
+                courses = json.load(json_file)
+                for department in courses:
+                    for course in department["courses"]:
+                        for section in course["sections"]:
+                            crns.append(section["crn"])
+
+            for crn in tqdm(crns, desc=term):
+                try:
+                    prerequisites[crn] = await get_prereq_string(s, term, crn)
+                except Exception as e:
+                    print(f"CRN: {crn} - {e}")
+                    prerequisites[crn] = {}
+
+            with open(f"data/{term}/prerequisites.json", "w") as outfile:
+                json.dump(prerequisites, outfile, indent=4)
 
 
 if __name__ == "__main__":
+    import asyncio
     import os
     from tqdm import tqdm
 
-    for term in tqdm(os.listdir("data"), desc="Semesters"):
-        prerequisites = {}
-
-        # For testing
-        # get_prereq_string(202009, 28329)
-        # exit()
-
-        crns = []
-        with open(f"data/{term}/courses.json") as json_file:
-            courses = json.load(json_file)
-            for department in courses:
-                for course in department["courses"]:
-                    for section in course["sections"]:
-                        crns.append(section["crn"])
-
-        for crn in tqdm(crns, desc=term):
-            print(crn)
-            try:
-                prerequisites[crn] = get_prereq_string(term, crn)
-            except Exception as e:
-                print(e)
-                prerequisites[crn] = {}
-
-        with open(f"data/{term}/prerequisites.json", "w") as outfile:
-            json.dump(prerequisites, outfile, indent=4)
+    asyncio.run(main())
