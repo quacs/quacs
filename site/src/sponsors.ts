2d8d697dca66b92d2114523d912207527010cb57
Benjamin Sherman <benjamin@bensherman.io>
2021-03-03 15:57:25 -0500

    Refactor sponsor information to be more-easily editable

---
+++ site/src/sponsors.ts
@@ -0,0 +1,18 @@
+import { Advert, LinkedSponsor } from "@/typings";
+
+export const advertisers: Advert[] = [
+  {
+    advertiser: "Cait for GM",
+
+    url: "https://www.caitforgm.com/home",
+    altText: "Cait for GM",
+    backgroundColor: "#2b517f",
+    desktop_path: "/cait_for_gm/desktop.png",
+    tablet_path: "/cait_for_gm/tablet.png",
+    mobile_path: "/cait_for_gm/mobile.png",
+  },
+];
+
+export const roboMallardPatrons: LinkedSponsor[] = [];
+
+export const rubberDuckPatrons: string[] = ["Enis Aras", "Chris Jerrett"];
