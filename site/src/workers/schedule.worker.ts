6062430638bad00eadb829a45b0d03d01a0dce10
Benjamin Sherman <benjamin@bensherman.io>
2021-04-21 19:04:30 -0400

    Refactor initialization waiting into separate function

--- site/src/workers/schedule.worker.ts
+++ site/src/workers/schedule.worker.ts
@@ -7,14 +7,18 @@ let ctx: WasmContext = null;
 // @ts-expect-error: after initialization, this won't be null
 let wasm: WasmType = null;
 
-function isInitialized(): boolean {
-  return (
-    wasm !== null &&
-    ctx !== null &&
+async function waitForInitialization(): Promise<void> {
+  while (
+    wasm === null ||
+    ctx === null ||
     // @ts-expect-error: For some reason, the wasm context is able to be constructed before
     // it's fully initialized... We need to check for that here by digging into its internals.
-    ctx.ptr !== 0
-  );
+    ctx.ptr === 0
+  ) {
+    await new Promise((resolve: (value?: unknown) => void) =>
+      setTimeout(resolve, 0)
+    );
+  }
 }
 
 export async function init(): Promise<void> {
@@ -30,11 +34,7 @@ export async function init(): Promise<void> {
 }
 
 export async function generateSchedulesAndConflicts(): Promise<number> {
-  while (!isInitialized()) {
-    await new Promise((resolve: (value?: unknown) => void) =>
-      setTimeout(resolve, 0)
-    );
-  }
+  await waitForInitialization();
 
   return wasm.generateSchedulesAndConflicts(ctx);
 }
@@ -43,31 +43,19 @@ export async function setSelected(
   crn: string,
   selected: boolean
 ): Promise<void> {
-  while (!isInitialized()) {
-    await new Promise((resolve: (value?: unknown) => void) =>
-      setTimeout(resolve, 0)
-    );
-  }
+  await waitForInitialization();
 
   return wasm.setSelected(ctx, parseInt(crn), selected);
 }
 
 export async function isInConflict(crn: number): Promise<boolean> {
-  while (!isInitialized()) {
-    await new Promise((resolve: (value?: unknown) => void) =>
-      setTimeout(resolve, 0)
-    );
-  }
+  await waitForInitialization();
 
   return wasm.isInConflict(ctx, crn);
 }
 
 export async function getSchedule(idx: number): Promise<Uint32Array> {
-  while (!isInitialized()) {
-    await new Promise((resolve: (value?: unknown) => void) =>
-      setTimeout(resolve, 0)
-    );
-  }
+  await waitForInitialization();
 
   return wasm.getSchedule(ctx, idx);
 }
