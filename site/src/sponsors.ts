e309404ced80e926a4a8eae19937d57a0c666d2a
Benjamin Sherman <benjamin@bensherman.io>
2021-10-19 22:04:15 -0400

    Add Bunkabl desktop advert

--- site/src/sponsors.ts
+++ site/src/sponsors.ts
@@ -1,6 +1,17 @@
 import { Advert, LinkedSponsor } from "@/typings";
 
-export const advertisers: Advert[] = [];
+export const advertisers: Advert[] = [
+  {
+    advertiser: "Bunkabl",
+
+    url: "https://bunkabl.com/",
+    altText: "Modern homes, brought to you by Bunkabl",
+    backgroundColor: "#2e755d",
+    desktop_path: "/bunkabl/desktop.png",
+    tablet_path: "/bunkabl/desktop.png",
+    mobile_path: "/bunkabl/desktop.png",
+  },
+];
 
 export const roboMallardPatrons: LinkedSponsor[] = [];
 
