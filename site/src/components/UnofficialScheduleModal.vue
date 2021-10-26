ab279c71f18daf19c5f0c3865d51064d7bcd7180
Eli Schiff <eli@elischiff.org>
2021-10-26 10:03:50 -0400

    Make 202201 semester official

--- site/src/components/UnofficialScheduleModal.vue
+++ site/src/components/UnofficialScheduleModal.vue
@@ -1,7 +1,7 @@
 <template>
   <b-modal
     title="Course offerings are not official!"
-    :visible="currentSem === '202201'"
+    :visible="currentSem === '202205'"
     centered
     size="xl"
     :hide-footer="!acceptedThatScheduleIsUnofficial"
