quacs/quacs

commit 79d91d75c023b08599298a02f549346ff63af81c

sis-scraper: rewrite as a login-less scraper
This change does a major refactoring of the sis-scraper
to no longer require logins to SIS by only using publicly
available sources. With the recent push to enable multi-factor
authentication on SIS in November (1), there was major concerns that the current
scraping system would not be usable. While workarounds exist, such as the
"Former Constitutent" system (2), there was no guarentee this would remain usable.

Additionally, removing the maintenance burdens associated with periodic
password resets and seemingly-random account lockages is a benefit of the new semester.
It also makes it easier for new contributors to test changes to the scraper, as they
no longer have to configure their credentials. Furthermore, this can enable better
regression testing in CI.

Current limitations/bugs:
1. credits per course is not scraped
2. courses with zero meeting times associated (independent studies mostly) don't show up
3. "closed" attribute is not being set. This can be brought back in a more limited form

(1) https://dotcio.rpi.edu/announcements/rss-sis-and-banner-forms-begin-requiring-multi-factor-authentication-faculty-and
(2) sis.rpi.edu/FormerConstituent.htm

scrapers/sis_scraper/conflict_logic.py
@@ -1,0 +1,207 @@
+# Python standard library
+from datetime import date
+from itertools import combinations
+import math
+import time
+
+
+def get_date(input_str):
+    """
+    Converts a date from SIS into the Python representation
+    """
+    return date.fromtimestamp(time.mktime(time.strptime(input_str, "%b %d, %Y")))
+
+
+def gen(term, data):
+    divide = range(1, 61)
+    unique_ranges = set()
+    end_date = date.today()
+    # Compute necessary preconditions for the optimization logic
+    for dept in data:
+        for course in dept["courses"]:
+            for section in course["sections"]:
+                for timeslot in section["timeslots"]:
+                    end_date = max(end_date, get_date(timeslot["dateEnd"]))
+                    start_date = get_date(timeslot["dateStart"])
+                    unique_ranges.add(start_date)
+                    divide = filter(
+                        lambda x: timeslot["timeStart"] % x
+                        == timeslot["timeEnd"] % x
+                        == 0,
+                        divide,
+                    )
+
+    MINUTE_GRANULARITY = max(divide)
+    NUM_MIN_PER_HOUR = 60 // MINUTE_GRANULARITY
+
+    # Generate binary conflict output
+    # day * 24 hours/day * NUM_MIN_PER_HOUR = total buckets
+    offset = lambda x: x * 24 * NUM_MIN_PER_HOUR
+
+    day_offsets = {
+        "M": offset(0),
+        "T": offset(1),
+        "W": offset(2),
+        "R": offset(3),
+        "F": offset(4),
+        "S": offset(5),
+        "U": offset(6),
+    }
+
+    BITS_PER_SLICE = offset(len(day_offsets))
+    BIT_VEC_SIZE = BITS_PER_SLICE * len(unique_ranges)
+
+    conflicts = {}
+    crn_to_courses = {}
+
+    # A table consisting of lists of classes that conflict on the 'x'th bit
+    sem_conflict_table = []
+
+    for _ in range(BIT_VEC_SIZE):
+        sem_conflict_table.append([])
+    for dept in data:
+        for course in dept["courses"]:
+            for section in course["sections"]:
+                conflict = [0] * BIT_VEC_SIZE
+                for time in section["timeslots"]:
+                    end = time["dateEnd"]
+                    start = time["dateStart"]
+                    my_end = get_date(end)
+                    my_start = get_date(start)
+                    for i, date_range in enumerate(unique_ranges):
+                        # check to see if we are in this range
+                        if my_end < date_range:
+                            continue
+                        if my_start > date_range:
+                            continue
+
+                        for day in time["days"]:
+                            for hour in range(0, 2400, 100):
+                                for minute in range(0, 60, MINUTE_GRANULARITY):
+                                    if (
+                                        time["timeStart"] <= hour + minute
+                                        and time["timeEnd"] > hour + minute
+                                    ):
+                                        minute_idx = minute
+                                        hour_idx = hour // 100
+                                        index = BITS_PER_SLICE * i + (
+                                            day_offsets[day]
+                                            + hour_idx * NUM_MIN_PER_HOUR
+                                            + minute_idx
+                                        )
+                                        conflict[index] = 1
+                                        sem_conflict_table[index].append(section["crn"])
+                if sum(conflict) == 0:
+                    continue
+                crn_to_courses[section["crn"]] = course["id"]
+                conflicts[section["crn"]] = conflict
+    # Compute unnecessary conflict bits - where a bit is defined as unnecessary if its removal does not affect the result conflict checking
+    # The following code computes a list of candidates that fit this criteria
+    unnecessary_indices = set()
+
+    for index1 in range(BIT_VEC_SIZE):
+        for index2 in range(index1 + 1, BIT_VEC_SIZE):
+            if (
+                index2 not in unnecessary_indices
+                and sem_conflict_table[index1] == sem_conflict_table[index2]
+            ):
+                unnecessary_indices.add(index2)
+
+    # Reverse the list as to not break earlier offsets
+    conflicts_to_prune = list(unnecessary_indices)
+    conflicts_to_prune.sort(reverse=True)
+
+    # Prune the bits in `conflicts_to_prune` from all the bitstrings
+    for section_crn in conflicts:
+        for bit in conflicts_to_prune:
+            del conflicts[section_crn][bit]
+
+    for x in conflicts_to_prune:
+        del sem_conflict_table[x]
+
+    BIT_VEC_SIZE -= len(unnecessary_indices)
+    unnecessary_indices.clear()
+
+    sem_conflict_dict = dict()
+
+    for index1 in range(BIT_VEC_SIZE):
+        for crn in sem_conflict_table[index1]:
+            if crn not in sem_conflict_dict:
+                sem_conflict_dict[crn] = set()
+
+            sem_conflict_dict[crn].add(index1)
+
+    # Optimization phase 2:
+    # Now that we're on a (greatly) reduced working space, we can now prune using this
+    # less efficient algorithm
+    for index1 in range(BIT_VEC_SIZE):
+        # We want all (unordered) pairs of conflicting courses on the bit `index1`
+        pair_list = [pair for pair in combinations(sem_conflict_table[index1], 2)]
+
+        # This part essentially tries to see if some other bit(s) other than the current one will create a conflict
+        # for the conflicting classes in pair_list
+        # If there is, we can safely discard this bit.
+        pairs_to_delete = set()
+        for pair in pair_list:
+            table1 = sem_conflict_dict[pair[0]]
+            table2 = sem_conflict_dict[pair[1]]
+            for x in table1:
+                if x != index1 and x in table2:
+                    pairs_to_delete.add(pair)
+
+        if len(pairs_to_delete) == len(pair_list):
+            for pair in pair_list:
+                table1 = sem_conflict_dict[pair[0]]
+                table2 = sem_conflict_dict[pair[1]]
+
+                table1.discard(index1)
+                table2.discard(index1)
+
+            unnecessary_indices.add(index1)
+
+    # Reverse the list as to not break earlier offsets
+    conflicts_to_prune = list(unnecessary_indices)
+    conflicts_to_prune.sort(reverse=True)
+
+    # Prune the bits in `conflicts_to_prune` from all the bitstrings
+    for section_crn in conflicts:
+        for bit in conflicts_to_prune:
+            del conflicts[section_crn][bit]
+
+        # Convert to a string for the quacs-rs rust codegen
+        conflicts[section_crn] = "".join(str(x) for x in conflicts[section_crn])
+
+    # Compute the proper bit vec length for quacs-rs
+    BIT_VEC_SIZE = math.ceil((BIT_VEC_SIZE - len(unnecessary_indices)) / 64)
+    with open(f"data/{term}/mod.rs", "w") as f:  # -{os.getenv("CURRENT_TERM")}
+        f.write(
+            """\
+//This file was automatically generated. Please do not modify it directly
+use ::phf::{{phf_map, Map}};
+pub const BIT_VEC_LEN: usize = """
+            + str(BIT_VEC_SIZE)
+            + """;
+pub static CRN_TIMES: Map<u32, [u64; BIT_VEC_LEN]> = phf_map! {
+"""
+        )
+
+        for crn, conflict in sorted(conflicts.items()):
+            rust_array = f"\t{crn}u32 => ["
+            for i in range(0, BIT_VEC_SIZE * 64, 64):
+                if i != 0:
+                    rust_array += ", "
+                rust_array += str(int(conflict[i : i + 64], 2))
+            rust_array += "],\n"
+
+            f.write(rust_array)
+
+        f.write(
+            """
+};
+pub static CRN_COURSES: Map<u32, &'static str> = phf_map! {
+"""
+        )
+
+        for crn, course in sorted(crn_to_courses.items()):
+            f.write(f'\t{crn}u32 => "{course}",\n')
+        f.write("};")

.gitignore
@@ -135,3 +135,7 @@ scrapers/*.msgpack
 scrapers/*/*.msgpack
 scrapers/*.rs
 scrapers/*/*.rs
+
+
+# Python
+*.pyc

scrapers/sis_scraper/.env-template
@@ -1,3 +1,0 @@
-RIN=
-PASSWORD=
-CURRENT_TERM=202009

scrapers/sis_scraper/util.py
@@ -1,0 +1,64 @@
+#!/usr/bin/env python3
+# Python standard library
+import datetime
+
+# "COMPUTER SCIENCE I" => "Computer Science I"
+def normalize_class_name(name):
+    name = list(name)
+
+    for i in range(1, len(name)):
+        if name[i - 1] == " ":
+            continue
+        name[i] = name[i].lower()
+    return "".join(name)
+
+
+# "11:00 AM - 1:00 PM" -> 1100,1300
+def timeToMilitary(time):
+    def __time_offset(time):
+        offset = 0
+        if "pm" in time and "12:" not in time:
+            offset = 1200
+        return int("".join(time.strip().split(":"))[:4]) + offset
+
+    if "TBA" in time:
+        return -1, -1
+    start, end = time.split("-")
+    return __time_offset(start), __time_offset(end)
+
+
+# 'Uzma   Mushtaque (P), Shianne M.  Hulbert ' -> Uzma Mushtaque, Shianne M. Hulbert
+def get_instructor_string(input):
+    return ", ".join(
+        [" ".join("".join(x.split("(P)")).split()) for x in input.split(",")]
+    )
+
+
+def get_semesters_to_scrape():
+    RPI_SEMESTER_MONTH_OFFSETS = {1, 5, 9}
+    semesters = []
+    date = datetime.date.today()
+    month = date.month
+    # roll back to nearest RPI start month
+    # We can get away with not needing time deltas since we can't wrap years
+    # due to January being a semester start month
+    while month not in RPI_SEMESTER_MONTH_OFFSETS:
+        month -= 1
+
+    date = datetime.date(date.year, month, 1)
+    semesters.append(date)
+
+    for _ in range(2):
+        # Now roll forward until we find two more semesters
+        # Since the maximum amount of days in a month is 31,
+        # we can add 32 to guarentee a jump, since timedelta has no months option :(
+        date += datetime.timedelta(days=32)
+        # Avoid overflowing months, just reset the day back to 1
+        date = date.replace(day=1)
+        while date.month not in RPI_SEMESTER_MONTH_OFFSETS:
+            date += datetime.timedelta(days=32)
+            date = date.replace(day=1)
+
+        semesters.append(date)
+
+    return [f"{sem.year}{str(sem.month).zfill(2)}" for sem in semesters]

commit a6e9a1adadf4c9cfe381ffb1156de773f14f1a8e

conflict_logic: minor improvements and optimizations
Fix 1:
Ben pointed something out to me in the code that ended up
being a non-issue. However, upon closer inspection, I found
we were needlessly building a set when we could just count
the number of redundant pairs and break early to avoid counting
duplicates

Fix 2:
I've been noticing some non-determinism in the bitpacks for the semesters.
Its very minor in fall2021 and spring2022. However, in Winter Enrichment 2021
its been very common. I believe I've isolated it to the unique_ranges set.
It's being iterated through and since sets have an undefined ordering
this could be the source of the problem. This is a regression caused
by 79d91d75c023b08599298a02f549346ff63af81c

Fix 3:
Minor improvement to the sis_scraper_test to report back information
about failures instead of simply printing AssertionError. It currently
still doesn't work for the winter enrichment term but that will come
soon.

scrapers/sis_scraper/conflict_logic.py
@@ -27,7 +27,8 @@ def gen(term, data):
                         divide,
                     )
                     """
-
+    # Sort to ensure deterministic ordering
+    unique_ranges = sorted(unique_ranges)
     MINUTE_GRANULARITY = 1
     NUM_MIN_PER_HOUR = 60 // MINUTE_GRANULARITY
 
@@ -141,15 +142,16 @@ def gen(term, data):
         # This part essentially tries to see if some other bit(s) other than the current one will create a conflict
         # for the conflicting classes in pair_list
         # If there is, we can safely discard this bit.
-        pairs_to_delete = set()
+        redundant_pairs = 0
         for pair in pair_list:
             table1 = sem_conflict_dict[pair[0]]
             table2 = sem_conflict_dict[pair[1]]
             for x in table1:
                 if x != index1 and x in table2:
-                    pairs_to_delete.add(pair)
+                    redundant_pairs += 1
+                    break
 
-        if len(pairs_to_delete) == len(pair_list):
+        if redundant_pairs == len(pair_list):
             for pair in pair_list:
                 table1 = sem_conflict_dict[pair[0]]
                 table2 = sem_conflict_dict[pair[1]]
@@ -173,7 +175,7 @@ def gen(term, data):
 
     # Compute the proper bit vec length for quacs-rs
     BIT_VEC_SIZE = math.ceil((BIT_VEC_SIZE - len(unnecessary_indices)) / 64)
-    with open(f"data/{term}/mod.rs", "w") as f:  # -{os.getenv("CURRENT_TERM")}
+    with open(f"data/{term}/mod.rs", "w") as f:
         f.write(
             """\
 //This file was automatically generated. Please do not modify it directly

scrapers/sis_scraper/sis_scraper_test.py
@@ -100,6 +100,7 @@ def bitpack_section_conflict(bp1, bp2):
 # courses. This compares the bitpack results to the results from neieve comparision
 def test_conflict_bitpacks():
     for term in os.listdir("data"):
+        print(f"Testing {term}")
         bitpacks = get_bitpacks(f"data/{term}/mod.rs")
         sections = []
         with open(f"data/{term}/courses.json") as f:
@@ -119,7 +120,18 @@ def test_conflict_bitpacks():
                 bitpack_conflict = bitpack_section_conflict(
                     bitpacks[section1["crn"]], bitpacks[section2["crn"]]
                 )
-            assert naive_conflict == bitpack_conflict
+            if naive_conflict != bitpack_conflict:
+                print(
+                    f"""ERROR!
+                    Naive algorithm says that {section1["crn"]} and {section2["crn"]} {"do" if naive_conflict else "do not"} have a conflict.
+                    ... but bitpacks indicate they {"do" if bitpack_conflict else "do not"}!
+
+                    Section 1 data:\n{section1}
+                    --------------------------
+                    Section 2 data:\n{section2}
+                    """
+                )
+                exit(1)
 
 
 if __name__ == "__main__":