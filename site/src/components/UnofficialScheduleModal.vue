8a0f0021e43646e180ccb8da47d2a55aa6633a77
John C. Allwein <git@johnnyapol.me>
2022-03-06 18:49:19 -0700

    chore: make Arch 2022 'official'

--- site/src/components/UnofficialScheduleModal.vue
+++ site/src/components/UnofficialScheduleModal.vue
@@ -1,7 +1,7 @@
 <template>
   <b-modal
     title="Course offerings are not official!"
-    :visible="currentSem === '202205'"
+    :visible="currentSem === '202209'"
     centered
     size="xl"
     :hide-footer="!acceptedThatScheduleIsUnofficial"
