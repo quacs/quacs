61412c4653acf8685294a00fafd50556e488a064
John C. Allwein <git@johnnyapol.me>
2022-03-16 21:14:58 -0600

    chore: make fall 2022 official

--- site/src/components/UnofficialScheduleModal.vue
+++ site/src/components/UnofficialScheduleModal.vue
@@ -1,7 +1,7 @@
 <template>
   <b-modal
     title="Course offerings are not official!"
-    :visible="currentSem === '202209'"
+    :visible="currentSem === '202301'"
     centered
     size="xl"
     :hide-footer="!acceptedThatScheduleIsUnofficial"
