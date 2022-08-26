5c74cac0321db579e495bea0d74fa383df1f4e84
Benjamin Sherman <benjamin@bensherman.io>
2022-08-25 20:05:22 -0400

    Remove reference to YACS from meta description for root site

--- site/scripts/build_entry.sh
+++ site/scripts/build_entry.sh
@@ -60,7 +60,8 @@ cat <<EOF >"$OUTPUT_DIR/index.html"
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <meta name="description" content="QuACS is the new and improved course scheduler for Rensselaer Polytechnic Institute (RPI) students. Stop using YACS and start using QuACS!">
+    <meta name="description" content="QuACS is the new and improved course scheduler for Rensselaer Polytechnic Institute students.  We're always up to date, and always accurate.">
+    <meta name="keywords" content="RPI,Rensselaer Polytechnic Institute,YACS,registration,course planning,planner">
     <meta name="og:image" content="/img/icons/android-chrome-512x512.png" />
     <link rel="icon" href="/favicon.ico">
     <meta http-equiv="Refresh" content="0; url='/$LATEST_SEMESTER'" />
