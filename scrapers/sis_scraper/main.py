83d26f562fb0597a2a96594bf9cc070944ab7875
3eef8a28f26fb2bcc514e6f1938929a1f931762 <116031952+3eef8a28f26fb2bcc514e6f1938929a1f931762@users.noreply.github.com>
2022-12-29 19:35:17 -0600

    The code actually works this time

--- scrapers/sis_scraper/main.py
+++ scrapers/sis_scraper/main.py
@@ -39,11 +39,10 @@ async def get_section_information(section_url):
         credit_data = (
             re.search(r"<br/>\n(.*?) Credits\n<br/>", str(soup)).group(1).strip()
         )
-        for delim in ["TO","OR"]:
-            if delim in credit_data:
-                credit_min, credit_max = (float(x.strip()) for x in credit_data.split(delim))
-            else:
-                credit_min = credit_max = float(credit_data)
+        
+        credit_data = list(map(float,re.split("TO|OR",credit_data)))
+        credit_min = min(credit_data)
+        credit_max = max(credit_data)
 
         section_dict["credMin"] = credit_min
         section_dict["credMax"] = credit_max
