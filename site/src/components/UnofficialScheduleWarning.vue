f1cd4ca7b1bc3d4fd0d292c762488aa21f0395e0
powe97 <116031952+powe97@users.noreply.github.com>
2023-10-16 14:26:03 -0400

    Convert inline CSS to themeable CSS vars

--- site/src/components/UnofficialScheduleWarning.vue
+++ site/src/components/UnofficialScheduleWarning.vue
@@ -2,19 +2,7 @@
   <div class="row" style="padding-top: 1rem">
     <div class="col-lg-1"></div>
     <div class="col-lg">
-      <div
-        style="
-          text-align: center;
-          padding-top: 0.5rem;
-          margin-left: 1rem;
-          margin-right: 1rem;
-          padding-bottom: 1rem;
-          font-size: 1.7rem;
-          border-style: solid;
-          border-width: 1px;
-          color: #ff1111;
-        "
-      >
+      <div class="unofficial-schedule-warning">
         QuACS is currently based off of incomplete data for this semester.<br />
         It is still largely a copy/paste of last year's schedule, and is heavily
         subject to change.<br />
