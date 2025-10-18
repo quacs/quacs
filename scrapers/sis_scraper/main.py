4c07072516116fb287674df9bc2336c554a3730e
powe97 <116031952+powe97@users.noreply.github.com>
2025-10-18 07:02:11 -0400

    Scrape whole term, also remove some debugging stuff

--- scrapers/sis_scraper/main.py
+++ scrapers/sis_scraper/main.py
@@ -54,18 +54,44 @@ session = None
 #   ],
 #   "title": "Introduction To Graphic Design"
 # }
-async def get_crn_details(term, crn):
+async def get_crn_information(term, crn):
     data = {}
 
     url = "https://sis9.rpi.edu/StudentRegistrationSsb/ssb/searchResults/{mode}"
     request_payload = {"term": term, "courseReferenceNumber": crn}
 
+    # I hate this. The *only* way I could find of getting the subject code
+    # was to scrape the bookstore link. Good lord.
+    async with session.post(
+        url.format(mode="getSectionBookstoreDetails"), data=request_payload
+    ) as req:
+        # Short circuit if we hit an error page, this happens if a CRN is unused
+        if req.status != 200:
+            return {}
+
+        soup = BeautifulSoup(await req.text())
+        data.update(
+            {
+                "subj": parse_qs(urlparse(soup.find_all("a")[0]["href"]).query)[
+                    "department1"
+                ][0]
+            }
+        )
+
     # Get basic course information. Notably excluded here is the subject code.
-    # No idea why, but I found a stupid workaround.
+    # Hence the above workaround.
     async with session.post(
         url.format(mode="getClassDetails"), data=request_payload
-    ) as html:
-        soup = BeautifulSoup(await html.text())
+    ) as req:
+        # Short circuit if we hit an error page, this happens if a CRN is unused
+        if req.status != 200:
+            return {}
+
+        soup = BeautifulSoup(await req.text())
+
+        # Sometimes the entire entry is blank. No idea why.
+        if not soup.find(id="courseReferenceNumber").text:
+            return {}
 
         # Get credit amount
         # If it is a variable number of credits, this span tag
@@ -84,6 +110,10 @@ async def get_crn_details(term, crn):
                 .strip()
             )
 
+        # Sometimes the credit string is empty. I don't know why either.
+        if not credit_data:
+            credit_data = (0,)
+
         credit_min = min(credit_data)
         credit_max = max(credit_data)
 
@@ -98,27 +128,17 @@ async def get_crn_details(term, crn):
             }
         )
 
-    # I hate this. The *only* way I could find of getting the subject code
-    # was to scrape the bookstore link. Good lord.
-    async with session.post(
-        url.format(mode="getSectionBookstoreDetails"), data=request_payload
-    ) as html:
-        soup = BeautifulSoup(await html.text())
-        data.update(
-            {
-                "subj": parse_qs(urlparse(soup.find_all("a")[0]["href"]).query)[
-                    "department1"
-                ][0]
-            }
-        )
-
     # Get course attributes. Note that we concatenate them into a string,
     # this is just for consistency with the old scraper. It is definitely
     # inefficient.
     async with session.post(
         url.format(mode="getSectionAttributes"), data=request_payload
-    ) as html:
-        soup = BeautifulSoup(await html.text())
+    ) as req:
+        # Short circuit if we hit an error page, this happens if a CRN is unused
+        if req.status != 200:
+            return {}
+        soup = BeautifulSoup(await req.text())
+
         attributes = [
             tag.text.strip().split("  ")[0]
             for tag in soup.find_all("span", {"class": "attribute-text"})
@@ -128,8 +148,12 @@ async def get_crn_details(term, crn):
     # Get course capacity
     async with session.post(
         url.format(mode="getEnrollmentInfo"), data=request_payload
-    ) as html:
-        soup = BeautifulSoup(await html.text())
+    ) as req:
+        # Short circuit if we hit an error page, this happens if a CRN is unused
+        if req.status != 200:
+            return {}
+        soup = BeautifulSoup(await req.text())
+
         regex_string = (
             r'<span class="status-bold">{param}:</span> <span dir="ltr">(.*?)</span>'
         )
@@ -157,7 +181,15 @@ async def get_crn_details(term, crn):
     # Get timeslots. This request returns JSON instead of HTML for some reason
     async with session.post(
         url.format(mode="getFacultyMeetingTimes"), data=request_payload
-    ) as html:
+    ) as req:
+        # Short circuit if we hit an error page, this happens if a CRN is unused
+        if req.status != 200:
+            return {}
+
+        json_data = json.loads(await req.text())["fmt"]
+        if not json_data:
+            return {}
+
         # This request gives us a ton of extraneous data, we drop most of it here
         data.update(
             {
@@ -165,29 +197,39 @@ async def get_crn_details(term, crn):
                     {
                         "dateEnd": e["meetingTime"]["endDate"],
                         "dateStart": e["meetingTime"]["startDate"],
-                        "timeStart": int(e["meetingTime"]["beginTime"]),
-                        "timeEnd": int(e["meetingTime"]["endTime"]),
+                        "timeStart": (
+                            int(e["meetingTime"]["beginTime"])
+                            if e["meetingTime"]["endTime"] is not None
+                            else -1
+                        ),
+                        "timeEnd": (
+                            int(e["meetingTime"]["endTime"])
+                            if e["meetingTime"]["endTime"] is not None
+                            else -1
+                        ),
                         # This is some of the grossest code I have ever written. Let me explain
                         # If there exists any faculty, then put the primary at the front of the
                         # list, and sort the remaining by last name. Then save the names
                         # with first name first, for consistency with older data.
                         "instructor": (
-                            [
-                                " ".join(f["displayName"].split(", ")[::-1])
-                                for f in list(
-                                    e
-                                    for e in e["faculty"]
-                                    if e["primaryIndicator"] == True
-                                )
-                                + sorted(
-                                    list(
+                            ", ".join(
+                                [
+                                    " ".join(f["displayName"].split(", ")[::-1])
+                                    for f in list(
                                         e
                                         for e in e["faculty"]
-                                        if e["primaryIndicator"] == False
-                                    ),
-                                    key=lambda d: d["displayName"],
-                                )
-                            ]
+                                        if e["primaryIndicator"] == True
+                                    )
+                                    + sorted(
+                                        list(
+                                            e
+                                            for e in e["faculty"]
+                                            if e["primaryIndicator"] == False
+                                        ),
+                                        key=lambda d: d["displayName"],
+                                    )
+                                ]
+                            )
                             if e["faculty"]
                             else ""
                         ),
@@ -207,14 +249,61 @@ async def get_crn_details(term, crn):
                                 ],
                             )
                         ),
-                        "location": f'{e["meetingTime"]["buildingDescription"]} {e["meetingTime"]["room"]}',
+                        "location": f'{e["meetingTime"]["buildingDescription"] or "TBA"} {e["meetingTime"]["room"] or ""}'.strip(),
                     }
-                    for e in json.loads(await html.text())["fmt"]
+                    for e in json_data
                 ]
             }
         )
     return data
 
+
+# Scrapes the term and dumps it to a file.
+# For now THE PREREQUISITE SCRAPING IS DISABLED.
+async def scrape_term(term):
+    print(f"Scraping term {term}...")
+    # This is stupid but so was the SIS update. For some reason,
+    # the normal course search requires cookies and a session ID,
+    # which we can't really do here. So my plan is to just scrape
+    # every single CRN a semester uses. There are many. Unfortunately,
+    # the range of CRNs isn't contiguous either-- there are a lot
+    # of holes, so we can't just try find an upper and lower bound.
+
+    # Due to the timeout on asyncio.gather, we need to do this in chunks.
+    # There will indeed be chunks with absolutely no data whatsoever.
+
+    data = []
+    for x in range(10, 100):
+        crn_low = x * 1000 + 1
+        crn_high = (x + 1) * 1000
+        data += list(
+            filter(
+                None,
+                await asyncio.gather(
+                    *[
+                        get_crn_information(term, crn)
+                        for crn in range(crn_low, crn_high)
+                    ]
+                ),
+            )
+        )
+        print(f"Scraped CRNs {crn_low} to {crn_high-1}")
+
+    # Now we need to restructure the data to match the courses.json format.
+    courses = [
+        {"code": subj, "courses": [
+            {
+                "crse": crse,
+                "id": f"{subj}-{crse}",
+                "sections": [ e for e in data if e["subj"] == subj and e["crse"] == crse ]
+            }
+            for crse in {e["crse"] for e in data if e["subj"] == subj}
+        ]}
+        for subj in {e["subj"] for e in data}
+    ]
+
+    with open(f"data/{term}/courses.json", "w") as outfile:
+        json.dump(courses, outfile, sort_keys=True, indent=2)
 
 
 async def main():
@@ -227,11 +316,6 @@ async def main():
         connector=aiohttp.TCPConnector(limit=5)
     ) as session:
         semesters = util.get_semesters_to_scrape()
-
-        print(await get_crn_details(202601, 36982))
-        print(await get_crn_details(202601, 38413))
-        print(await get_crn_details(202601, 37074))
-        return
 
         if sys.argv[-1] == "ALL_YEARS":
             print("Parsing all years")
